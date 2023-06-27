dfb9c1aef9e92c5f3693814285809c38913b38fc
Ruijian Cao <caoruijian@big.one>
2023-06-27 10:49:28 +0800

    add script

--- NOTE.md
+++ NOTE.md
@@ -1,3 +1,27 @@
 # Notes 
 
+## Escript
+
 http://davekuhlman.org/elixir-escript-mix.html
+
+## Http clients
+
+https://github.com/ninenines/gun
+https://github.com/benoitc/hackney
+https://github.com/wojtekmach/req
+
+## Proxy
+
+https://github.com/rofl0r/proxychains-ng
+
+### Http to socks5 proxy
+
+https://github.com/KaranGauswami/socks-to-http-proxy
+
+
+https://github.com/oyyd/http-proxy-to-socks
+
+```
+npm install -g http-proxy-to-socks
+hpts -s 127.0.0.1:1080 -p 8080
+```
--- lib/ecli.ex
+++ lib/ecli.ex
@@ -34,6 +34,35 @@ defmodule Ecli do
             {:ok, _} = Application.ensure_all_started(:req)
             req = Eclient.get!(url)
             output("body: \n#{req.body |> inspect}")
+
+          "proxy" ->
+            if length(args) < 2 do
+              raise "require url in req action, like https://api.github.com/repos/elixir-lang/elixir"
+            end
+
+            {:ok, _} = Application.ensure_all_started(:req)
+            url = Enum.at(args, 1)
+
+            req_opts = [
+              connect_options: [
+                transport_opts: [
+                  # Issue: (ArgumentError) unknown application: :castore in escript
+                  # default verify strategy
+                  # verify: :verify_peer
+                  # or use
+                  # verify: :verify_none
+                  # cacerts: :public_key.cacerts_get()
+                  verify: :verify_none
+                ],
+                # not works direct with socks5h here!!!
+                # use below from https://github.com/oyyd/http-proxy-to-socks
+                # hpts -s 127.0.0.1:1080 -p 8080
+                proxy: {:http, "localhost", 8080, []}
+              ]
+            ]
+
+            req = Req.get!(url, req_opts)
+            output("body: \n#{req.body |> inspect}")
         end
     end
   end
